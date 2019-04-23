4025301e19467f2979aaf2d76cf91b21f31f3a6c
Brandon <32998909+vnkbrand@users.noreply.github.com>
2019-04-23 16:09:10 +0700

    Updates

---
+++ precache-manifest.785a5b5b889f30b5ba32d0f07414a2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "7c302e8cee8817525838",
+    "url": "/contactmanager/static/js/main.8e31e0cf.chunk.js"
+  },
+  {
+    "revision": "24b640ee26d1e866eb47",
+    "url": "/contactmanager/static/js/2.8968a7eb.chunk.js"
+  },
+  {
+    "revision": "7c302e8cee8817525838",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "24b640ee26d1e866eb47",
+    "url": "/contactmanager/static/css/2.aa3da87b.chunk.css"
+  },
+  {
+    "revision": "4353ae64f41448b8dab6702ba092ecb0",
+    "url": "/contactmanager/index.html"
+  }
+];
